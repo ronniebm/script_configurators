736dccf0ba61f025ff5ddc36cff9d42d9e5b08c0
ronniebm <ronnie.coding@gmail.com>
2020-07-15 12:39:16 -0500

    betty install v1.1 hfx32

--- a_config.sh
+++ a_config.sh
@@ -129,7 +129,7 @@ install_zsh()
 			VAR1="n";
 			sudo apt-get install zsh;
 			wait
-			sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+			curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 			# chsh -s $(which zsh);
 			# cp zsh_assets/.p10k.zsh $HOME/.;
 			# cp zsh_assets/.zshrc $HOME/.;
