7ab5bf6bf4cc2c686ef524eedf8521fbb6108799
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-16 23:40:26 -0500

    promp new work done

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ PROGRAM_LIST=(
 	mysql
 	vim
 	emacs
+	caracoles
+	ereslomeju
 )
 #--------auto generate var promp dict (only for bash v4.0.0 or high------------
 declare -A PROMP_DICT
@@ -29,6 +31,9 @@ function prog_validator()
 {
 	FILE1=/$HOME/.git-credentials
 	COUNTER=0
+	RED="\033[0;31m"
+	GREEN="\033[0;32m"
+	NC="\033[0m"
 	BETTY_SKIP=0
 	len=${#PROGRAM_LIST[@]}
 
@@ -43,42 +48,42 @@ function prog_validator()
 			then
 				CTRL_DICT["${PROGRAM_LIST[i]}"]="INSTALLED";
 				PROMP_DICT["${PROGRAM_LIST[i]}"]="${GREEN}INSTALLED${NC}";
-				echo "match"
 
 			else
 				CTRL_DICT["${PROGRAM_LIST[i]}"]="NOT FOUND";
 				PROMP_DICT["${PROGRAM_LIST[i]}"]="${RED}NOT FOUND${NC}";
-				echo "NO match "${PROGRAM_LIST[0]}" "
 			fi
 		fi
 	done
 }
 
-function prompt()
+function prompt_two()
 {
-#clear;
+len=${#PROGRAM_LIST[@]}
+clear;
 echo 	'*********************************************************';
-echo 	'*   My personal configurator v1.0 (by Ronnie B.M.)      *';
+echo 	'* My personal configurator v1.0 (by Ronnie B.M. Jose V) *';
 echo 	'* ===================================================== *';
 echo 	'* use it by your OWN RISK, tested on UBUNTU 18 bionic   *';
-echo 	'*                                                       *';
+echo 	'* bash v4.0                                             *';
 echo 	'* Github: ronniebm,  Email: ronnie.coding@gmail.com     *';
 echo 	'* ----------------------------------------------------- *';
 echo 	'*                                                       *';
 echo 	'*  0. Automatic Installation                            *';
-echo -e "*  1. betty (C code style validator) ..[ "${PROMP_DICT[betty]}" ]    *";
-echo -e "*  2. zsh (Oh my Zsh shell) ...........[ "${PROMP_DICT[zsh]}" ]    *";
-echo -e "*  3. git .............................[ "${PROMP_DICT[git]}" ]    *";
-echo -e "*  5. shellcheck validator ............[ "${PROMP_DICT[shellcheck]}" ]    *";
-echo -e	"*  6. valgrind memory tester ..........[ "${PROMP_DICT[valgrind]}" ]    *";
-echo -e	"*  7. MYSQL database ..................[ "${PROMP_DICT[mysql]}" ]    *";
-echo -e	"*  8. VIM   [customized] ..............[ "${PROMP_DICT[vim]}" ]    *";
-echo -e	"*  9. EMACS [customized] ..............[ "${PROMP_DICT[emacs]}" ]    *";
+
+for ((i=0; i<"$len"; i++))
+do
+	tool="${PROGRAM_LIST[i]}"
+	tool_state="${PROMP_DICT["$tool"]}"
+	echo -e "*  "$i"."$tool"\t.......................[ "$tool_state" ]    *";
+
+done
+
 echo 	'*                                                       *';
 echo 	'*********************************************************';
 echo 	'';
 }
 
-prog_validator
-prompt
 
+prog_validator
+prompt_two
