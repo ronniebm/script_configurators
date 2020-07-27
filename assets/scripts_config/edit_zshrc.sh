9b38de34dbdc6d965d33bfeffa7fd599a8c989d6
ronniebm <ronnie.coding@gmail.com>
2020-07-27 11:18:31 -0500

    removing file test.sh

---
+++ assets/scripts_config/edit_zshrc.sh
@@ -0,0 +1,11 @@
+if [ -f "$HOME/.zshrc" ]
+then
+	sed -i "s/ plugins=(git)/ plugins=(git zsh-autosuggestions zsh-syntax-highlighting colored-man-pages)/" $HOME/.zshrc;
+fi
+
+
+==   ==   =====   ==       =====
+||   ||  ||   ||  ||      ||  //
+||===||  ||   ||  ||      ||===
+||   ||  ||   ||  ||      ||    ))
+==   ==   =====    =====   =====
