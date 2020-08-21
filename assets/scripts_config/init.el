56bd0e6a291d954142d7878800f5306893fe2cc6
ronniebm <ronnie.coding@gmail.com>
2020-08-20 22:43:39 -0500

    powerfull init.el version_1

--- assets/scripts_config/init.el
+++ assets/scripts_config/init.el
@@ -1,171 +1,101 @@
-;; ***************************************
-;; ** My personal EMACS Configuration.  **
-;; ** --------------------------------  **
-;; ** author: ronnie barrios            **
-;; ** github: ronniebm                  **
-;; ** email:  ronnie.coding@gmail.com   **
-;; ** --------------------------------  **
-;; ** feel free to send me an email !   **
-;; ***************************************
+;; ======== RONNIE EMACS Customization init.el === ronnie.coding@gmail.com ========================
 ;;
-;; EMACS TESTED VERSIONS:
-;;   -> GNU Emacs 24.3.1
-;;   -> ...
-;; ---------------------------------------
-;;
-;; OS TESTED VERSIONS:
-;;   -> Ubuntu, 14.04.6 LTS - Trusty Tahr.
-;;   -> Ubuntu, 18.04 LTS - Bionic Beaver.
-;; ---------------------------------------
+;; Tested:  Emacs 24.3 (ubuntu_14), Emacs 25.2 (ubuntu_18)
+;; ================================================================================================
 
-;; When Emacs is started, it normally tries to load a Lisp program from an
-;; initialization file, or init file for short. This file, if it exists,
-;; specifies how to initialize Emacs for you. Emacs looks for your init file
-;; using the filenames ~/.emacs, ~/.emacs.el, or ~/.emacs.d/init.el; you can
-;; choose to use any one of these three names.
-;;
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Init-File.html
-;; -------------------------------------------------------------------------
+; SHORT SETTINGS ----------------------------------------------------------------------------------
+(package-initialize)                                          ;; package initialize.
+(setq inhibit-startup-screen t)                               ;; dissable startup screen.
+(menu-bar-mode -1)                                            ;; dissable menu bar.
+(tool-bar-mode -1)                                            ;; dissable tool bar.
+(scroll-bar-mode -1)                                          ;; dissable scroll bar.
+(if (display-graphic-p)(set-background-color "#aaaaaa"))     ;; if GUI, set bckg.color.
+(set-background-color "honeydew")
+(set-language-environment "UTF-8")                            ;; set language UTF-8.
+(setq initial-scratch-message "== My notes ==\n\n> ")         ;; *scratch* screen title.
+(blink-cursor-mode -1)                                        ;; blink cursor OFF.
+(global-set-key (kbd "RET") 'newline-and-indent)              ;; new-line-and-intent.
+(setq make-backup-files nil)                                  ;; prevent bakup files ~
+(global-hl-line-mode 1)                                       ;; highlight current line.
+(set-face-background 'hl-line "#e3e229")                      ;; face backgrount line.
+(line-number-mode 1)                                          ;; show line num bottom bar.
+(global-linum-mode t)                                         ;; show line num left-colum.
+(setq linum-format " %3d ")                                   ;; line num lef-colum format.
 
-
-;; 1) Add Main Repositories for Emacs packages (work only in ubuntu 14).
-;; (package-initialize)
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;			 ("marmalade" . "http://marmalade-repo.org/packages/")
-;;			 ("melpa" . "http://melpa.org/packages/")))
-;;
-;;
-;; =========================================================================
-;; MELPA Package Support
-;; =========================================================================
-;; Enables basic packaging support:
-
-(require 'package)
-
-;; Adds the Melpa archive to the list of available repositories
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-
-;; Initializes the package infrastructure
-(package-initialize)
-
-;; If there are no archived package contents, refresh them
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; ===============================
-;; Installs packages
-;; ===============================
-;; myPackages contains a list of package names:
-;;
-(defvar myPackages
-  '(better-defaults                 ;; Set up some better Emacs defaults
-    elpy                            ;; Emacs Lisp Python Environment
-    flycheck                        ;; On the fly syntax checking
-    py-autopep8                     ;; Run autopep8 on save
-    blacken                         ;; Black formatting on save
-    material-theme                  ;; Theme
-    neotree                         ;; file explorer bar
-    )
-)
-
-;; ===============================
-;; Scans the list in myPackages.
-;; ===============================
-;; If the package listed is not already installed, install it
-(mapc #'(lambda (package)
-	  (unless (package-installed-p package)
-	    (package-install package)))
-      myPackages)
-
-;; laad material theme.
-(load-theme 'material t)
-
-;;
-;; ==============================
-;; PYTHON Development Setup
-;; ==============================
-;; Enable elpy
-(elpy-enable)
-
-;; Enable Flycheck
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-;; Enable autopep8
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-;; ==============================================================
-;; MY PERSONAL CONFIGURATION SETTINGS
-;; ==============================================================
-;; 1) Removes toolbar, scroll bar and menu bar.
-;;    to make emacs looks less cluttered.
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-
-;; 2) Inhibit startup-message and scratch-message.
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
-;; 3) blink cursor mode adjustment and new line autoindent.
-(blink-cursor-mode -1)
-(global-set-key (kbd "RET") 'newline-and-indent)
-
-;; 4) prevent backup files ~ from being created.
-(setq make-backup-files nil)
-
-;; 5) defining my aliases access Alt-x + 'alias_name'.
-(defalias 'themes 'customize-themes)
-(defalias 'lines 'list-matching-lines)
-
-;; 6) Emacs Highlight the Current Line.
-(global-hl-line-mode 1)
-(set-face-background 'hl-line "#557c80")
-
-;; 7) Turn ON 'show Line Number' in the bottom bar.
-(line-number-mode 1)
-
-;; 8) Configuring left column numerating feature: 'Linum-mode'.
-(global-linum-mode t)
-(setq linum-format " %3d ")  ;my personal format
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(linum ((t (:inherit (shadow default) :background "#23adac" :foreground "#ffffff")))))
+ '(linum ((t (:background "#23adac" :foreground "#ffffff")))))
 
-;; 9) customized-theme:
-(load-theme 'tsdh-dark 1)
+(load-theme 'tsdh-dark 1)                                     ;; customized theme.
+(setq show-paren-delay 0)                                     ;; show matching parenthesis.
+(show-paren-mode t)                                           ;; show parent mode.
+(electric-pair-mode 1)                                        ;; show pairs () [] {} y “”.
+(global-visual-line-mode t)                                   ;; show proper line wrapping.
+(setq-default show-trailing-whitespace t)                     ;; show trailing whitespaces.
+(delete-selection-mode 1)                                     ;; delete/overwrite over selected text.
+(electric-indent-mode 0)                                      ;; electric indent mode OFF.
+(set-default 'tab-always-indent 'complete)                    ;; tab always indent ON.
+(setq-default indent-tabs-mode nil)                           ;; indent-tabs-mode DISSABLED.
+(setq-default tab-width 4)                                    ;; tab-with is 4.
+(set-face-attribute 'default nil :height 130)                 ;; Default font size = 13spx.
 
-;; 10) Highlight corresponding parentheses when cursor is on one.
-(setq show-paren-delay 0) ;; shows matching parenthesis asap
-(show-paren-mode t)
-(electric-pair-mode 1)    ;; it pairs () [] {} y “”.
 
-;; 11) Proper line wrapping.
-(global-visual-line-mode t)
 
-;; 12) Shows innecessart trailing white-spaces.
-(setq-default show-trailing-whitespace t)
+; ALIASES ----------------------------------------------------------------------------------------
+(defalias 'themes 'customize-themes)                          ;; access to themes config.
+(defalias 'replace 'replace-string)                           ;; replace strings in a document.
+(defalias 'lines 'list-matching-lines)                        ;; show all lines containing a str.
 
-;; ==============================================================
-;; 13) Remove useless whitespace when saving a file [Disabled].
-;; ==============================================================
-;;(add-hook 'before-save-hook 'whitespace-cleanup)
-;;(add-hook 'before-save-hook (lambda() (delete-trailing-whitespace)))
-;;
-;;
-;; ==============================================================
-;; 14) Folder explorer (file tree) Neotree [Disabled]
-;; ==============================================================
-;;(require 'neotree)
-;;(global-set-key [f8] 'neotree-toggle) ;; the F8 key toggles the file tree
-;;
-;;
+
+
+; KEY-BINDS --------------------------------------------------------------------------------------
+(global-set-key [M-delete] 'kill-whole-line)        ;; delete actual line.
+(global-set-key [f12] 'delete-trailing-whitespace)  ;; delete trailing whitspc.
+
+
+;; MY FUNCTIONS ----------------------------------------------------------------------------------
+
+(defun executable() ;; == EXEC. PERMISSIONS FOR CURRENT FILE ===
+  "Add executable permissions on current file."
+  (interactive)
+  (when (buffer-file-name)
+    (set-file-modes buffer-file-name
+		    (logior (file-modes buffer-file-name) #o100))
+    (message (concat "Made " buffer-file-name " executable"))))
+
+
+
+(defun lorem() ;; === MY LOREM IPSUM GENERATOR ===
+  "Insert a lorem ipsum."
+  (interactive)
+  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
+	  "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
+	  "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
+	  "aliquip ex ea commodo consequat. Duis aute irure dolor in "
+	  "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+	  "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
+	  "culpa qui officia deserunt mollit anim id est laborum."))
+
+
+
+(defun readme() ;; === MY README.md TEMPLATE ===
+  "Insert a README.md template."
+  (interactive)
+  (insert "<img src=\"https://camo.githubusercontent.com/04a8a9a456b8ecafad2eb4f2cff6803cd0194496/687474703a2f2f7777772e686f6c626572746f6e7363686f6f6c2e636f6d2f686f6c626572746f6e2d6c6f676f2e706e67\"
+width=30%/>  \n\n\n"
+	  "### PROJECT:  \n"
+	  "---\n\n\n"
+-	  "For this project, we are going to learn about:<br>\n"
+	  "- \n"
+	  "- \n\n\n"
+	  "#### GENERAL OBJECTIVES:<br>\n"
+	  "- \n"
+	  "- \n\n\n"
+	  "#### MY PERSONAL REVIEW/NOTES:<br>\n\n"
+	  "These are important things I want to document.\n\n\n\n"
+	  "---\n"
+	  "git_user: ronniebm  |  email: ronnie.coding@gmail.com\n"
+	  ))
+
 ;; ==============================================================
 ;; 15) Make Lines moves UP/DOWN, with M-up / M-down key-binds.
 ;; this is a very interesting feature !.. also it works if
@@ -209,112 +139,143 @@
 (defun move-text-up (arg)
   "Move region (transient-mark-mode active) or current line
   arg lines up."
-  (move-text-internal (- arg)))
-
   (interactive "*p")
+  (move-text-internal (- arg)))
 
 (global-set-key [M-up] 'move-text-up)         ;; moves a line up.
 (global-set-key [M-down] 'move-text-down)     ;; moves a line down.
-(global-set-key [M-delete] 'kill-whole-line)  ;; deletes actual line.
-
-;; ==========================================================================
-;; 16) Defining a Lorem ipsum generator function !.
-;; ==========================================================================
-;; for use press M-x lorem [Return] and the text
-;; should be placed ! cool isn't ?
-
-(defun lorem ()
-  "Insert a lorem ipsum."
-  (interactive)
-  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
-	  "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
-	  "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-	  "aliquip ex ea commodo consequat. Duis aute irure dolor in "
-	  "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-	  "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
-	  "culpa qui officia deserunt mollit anim id est laborum."))
-
-;; ==========================================================================
-;; 17) DEFINING MY PERSONAL ABBREVIATIONS.
-;; ==========================================================================
-;;
-
-(setq-default abbrev-mode t)
-(clear-abbrev-table global-abbrev-table)
-(define-abbrev-table 'global-abbrev-table
-  '(
-    ;; my personal abbrev
-    ("docstring" "\"\"\"by the way
-	      this is a
-	      long example !!" )
-    ("templatereadme" "### PROJECT:
----
-For this project, we are going to learn about:<br>
-
--
--
-
-#### GENERAL OBJECTIVES:<br>
-
--
--
-
-&emsp;&emsp;<img src=\"https://www.enter.co/wp-content/authors/Holberton-215.jpg\" />
----
-### Personal Review:
--
-
-
----
-git_user: ronniebm  |  email: ronnie.coding@gmail.com
-")
-    ;;
-    ))
 
 
 
 
-;; ===================================================================
-;; 18) Giving execution permissions to my current file !.
-;; ===================================================================
+; LONG SETTINGS ----------------------------------------------------------------------------------
 
-(defun executable()
-  "Add executable permissions on current file."
-  (interactive)
-  (when (buffer-file-name)
-    (set-file-modes buffer-file-name
-		    (logior (file-modes buffer-file-name) #o100))
-    (message (concat "Made " buffer-file-name " executable"))))
+;; === SET EMACS DEFAULT FONT (WINDOWS, MAC, or GNU/LINUX) ===
+
+(set-frame-font
+ (cond
+  ((string-equal system-type "windows-nt") ; Microsoft Windows
+   (if (member "Consolas" (font-family-list))
+       "Consolas"
+     nil
+     ))
+  ((string-equal system-type "darwin") ; macOS
+   (if (member "Menlo" (font-family-list))
+       "Menlo-14"
+     nil
+     ))
+  ((string-equal system-type "gnu/linux") ; linux
+   (if (member "DejaVu Sans Mono" (font-family-list))
+       "DejaVu Sans Mono"
+     nil
+     ))
+  (t nil))
+ t t)
 
 
-;; ===================================================================
-;; 19) Key-binds to resize buffer windows. [TESTING -- not released]
-;; ===================================================================
-;;(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
-;;(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-;;(global-set-key (kbd "S-C-<up>") 'enlarge-indow)
-;;(global-set-key (kbd "S-C-<down>") 'shrink-window)
+; ***********************************************************************************************
+; PACKAGES CONFIGURATIONS (Applyed on Emacs version == 24)    |   Ronnie's Personal Setup.
+; ***********************************************************************************************
 
-;; ===================================================================
-;; 20) Configuring a SHELL BUFFER in a bottom windows when emacs start.
-;;     [TESTING -- not released]
-;; ===================================================================
-;;(split-window-below 30)
-;;(other-window)
+(when (= emacs-major-version 24)
 
-(set-face-attribute 'default nil :height 150)
-(toggle-frame-fullscreen)
+  ; warning-minimum-level disabled --------------
+  (setq warning-minimum-level :emergency)
 
-(require 'neotree)
+  ; require/install packages --------------------
+  (require 'package)
+  (add-to-list 'package-archives
+               '("MELPA Stable" . "http://stable.melpa.org/packages/") t)
 
-;; Enable NEOTREE key-bind command.
-(global-set-key [f8] 'neotree-toggle)
+  (add-to-list 'package-archives
+               '("http://elpa.gnu.org/packages/") t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.(require 'neotree)
- '(package-selected-packages
-   (quote
-    (neotree py-autopep8 material-theme flycheck elpy blacken better-defaults))))
+
+  ; packages initialization ---------------------
+  (package-initialize)
+
+  ; creating a packages list. --------------------------------------------
+  (defvar myPackages
+    '(better-defaults                 ;; Set up some better Emacs defaults
+      flycheck                        ;; on the fly syntax checking
+      all-the-icons                   ;; install all-the-icons package
+      neotree                         ;; file explorer bar
+   ;   magit                           ;; emacs github features
+      )
+    )
+
+  ; installing packages. -------------------------------------------------
+
+  (mapc #'(lambda (package)
+	  (unless (package-installed-p package)
+	    (package-install package)))
+      myPackages)
+
+
+  ; some settings/key-binds for packages -----------
+  (global-set-key [f8] 'neotree-toggle)
+  (setq flycheck-mode 1)
+
+ )
+
+
+; ***********************************************************************************************
+; PACKAGES CONFIGURATIONS (Applyed on Emacs version == 25)    |   Ronnie's Personal Setup.
+; ***********************************************************************************************
+
+(when (= emacs-major-version 25)
+
+
+  ; configuring melpa 'stable' repository --------------------------------
+
+  (require 'package)
+
+  (add-to-list
+   'package-archives
+;   '("melpa" . "https://stable.melpa.org/packages/")
+   '("melpa" . "https://melpa.org/packages/")
+   t)
+
+
+  ; creating a packages list. --------------------------------------------
+
+  (defvar myPackages
+    '(better-defaults                 ;; Set up some better Emacs defaults
+      elpy                            ;; Emacs Lisp Python Environment
+      flycheck                        ;; On the fly syntax checking
+      py-autopep8                     ;; Run autopep8 on save
+      blacken                         ;; Black formatting on save
+      all-the-icons                   ;; install all-the-icons package
+      neotree                         ;; file explorer bar
+      auto-complete-c-headers
+      anaconda-mode
+      ac-c-headers
+      ac-html
+      )
+    )
+
+  ; installing packages. -------------------------------------------------
+
+  (mapc #'(lambda (package)
+	  (unless (package-installed-p package)
+	    (package-install package)))
+      myPackages)
+
+  ; settings -------------------------------------------------------------
+
+  (global-flycheck-mode 1)
+
+  (elpy-enable)
+
+  (setq elpy-modules
+        (delq 'elpy-module-flymake elpy-modules))
+
+  (add-hook 'elpy-mode-hook 'flycheck-mode)
+
+  (require 'py-autopep8)
+  (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+  (require 'neotree)
+  (global-set-key [f8] 'neotree-toggle)
+
+  )
